f34bfedea646dfc6ccf61d9fe9121c953ecce590
IlyaShkvalniy <96240610+IlyaShkvalniy@users.noreply.github.com>
2022-02-08 17:58:01 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -38,5 +38,5 @@ const MORSE_TABLE = {
     '     ':  ' '
 };
 
-const decode = expr =>   expr.split('').map( (item, i, arr ) => i % 2 === 0? item = item + arr[i + 1] : item).filter( item => item.length > 1).map( item => item === '00' ? item = '' : item === '**' ? item = ' ' : item === '10' ? item = '.' : item === '11' ? item = '-' : item).map( ( item => [item])).map((item , i ,arr) => i % 5 === 0 ? item : arr[i - (i %5)].push(item)).map( item => item.length > 1? item.flat(): item ).filter( item => item.length > 1).map( item => item.reduce( (acc, cur) => acc + cur)).map( item => item = MORSE_TABLE[item]).join('');
+const decode = expr => expr.split('').map( (item, i, arr ) => i % 2 === 0? item = item + arr[i + 1] : item).filter( item => item.length > 1).map( item => item === '00' ? item = '' : item === '**' ? item = ' ' : item === '10' ? item = '.' : item === '11' ? item = '-' : item).map( ( item => [item])).map((item , i ,arr) => i % 5 === 0 ? item : arr[i - (i %5)].push(item)).map( item => item.length > 1? item.flat(): item ).filter( item => item.length > 1).map( item => item.reduce( (acc, cur) => acc + cur)).map( item => item = MORSE_TABLE[item]).join('');
 module.exports = {decode}
